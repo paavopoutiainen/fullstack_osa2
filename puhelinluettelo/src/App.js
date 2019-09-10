be2b21f02c16b56cc1170c2e776a0fc62d005574
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2019-09-10 18:31:11 +0300

    step6

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,17 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from "./components/Filter"
 import AddForm from "./components/AddForm"
 import Contacts from "./components/Contacts"
+import axios from "axios"
+
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons] = useState([]) 
   const [person, setPerson] = useState({name:"", number:""})
   const [searchWord, setWord] = useState("")
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => 
+        setPersons(response.data))
+  },[])
 
   function addContact(e){
     e.preventDefault()
